1161e2f8de0e6e73b98bee1d0dec75a0fbba3391
RebJaenus <bberglund@comcast.net>
2019-07-14 17:59:57 +0000

    added link to new movie form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,4 +7,16 @@ class MoviesController < ApplicationController
     @movie = Movie.new
   end
 
+  def create
+    Movie.create(movie_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :genre, :format)
+  end
+
+
 end
